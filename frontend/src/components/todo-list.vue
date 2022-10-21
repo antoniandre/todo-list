fbe73f7db9af577d636781d3755338424df3071e
antoniandre <antoniandre.web@gmail.com>
2022-10-21 16:45:18 +0200

    Display all the tasks from the API.

--- frontend/src/components/todo-list.vue
+++ frontend/src/components/todo-list.vue
@@ -1,15 +1,44 @@
 <template>
-<div>
+<div class="todo-list">
   <h1>TODO LIST</h1>
+
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      <input type="checkbox" :checked="task.completed === 1">
+      <span>{{ task.label }}</span>
+    </li>
+  </ul>
 </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    tasks: []
+  }),
 
+  created () {
+    fetch('/api/', { method: 'get' })
+      .then(response => response.json())
+      .then(response => {
+        this.tasks = response
+      })
+
+    // fetch('/api/', {
+    //   method: 'post',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({label: 'hello', completed: 1})
+    // })
+    //   .then(response => {
+    //     console.log(response)
+    //   })
+  }
 }
 </script>
 
 <style>
-
+.todo-list ul {
+  list-style-type: none;
+  text-align: left;
+}
 </style>
