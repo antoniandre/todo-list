306e63675afabf176a211c283ade5b0a160b85b1
antoniandre <antoniandre.web@gmail.com>
2022-10-28 17:15:17 +0200

    Add ESLint and Sass.

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-empty': 'warn',
+    'no-unused-vars': 'warn',
+    'brace-style': ['warn', 'stroustrup']
+  }
+}
