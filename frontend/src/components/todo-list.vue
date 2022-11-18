b24a8b5019238d83d3e53d28719d02f84a0d4952
antoniandre <antoniandre.web@gmail.com>
2022-11-18 20:53:30 +0100

    Improve the layout & add a new task field.

--- frontend/src/components/todo-list.vue
+++ frontend/src/components/todo-list.vue
@@ -2,16 +2,24 @@
 <div class="main-content todo-list">
   <h1>To Do List</h1>
   <ul>
-    <li v-for="task in tasks" :key="task.id">
-      <input
-      v-model="task.completed"
-      @input="saveTask(task)"
-      :id="`checkbox-${task.id}`"
-      type="checkbox"
-      :checked="task.completed">
+    <li
+      v-for="task in tasks"
+      :key="task.id"
+      @click="saveTask(task)"
+      :class="{ checked: task.completed }">
       <i :class=" task.completed ? 'i-checkbox-checked' : 'i-checkbox-unchecked'"></i>
-      <label :for="`checkbox-${task.id}`">{{ task.label }}</label>
-      <router-link :to="`/task/${task.id}`" class="arrow">&#10132;</router-link>
+      <label>{{ task.label }}</label>
+      <router-link :to="`/task/${task.id}`" class="arrow i-arrow-right"></router-link>
+    </li>
+    <li @click="newTask.completed = !newTask.completed">
+      <i :class="newTask.completed ? 'i-checkbox-checked' : 'i-checkbox-unchecked'"></i>
+      <input
+        v-model="newTask.label"
+        @click.stop
+        @keypress.enter="saveNewTask(newTask)"
+        type="text"
+        placeholder="New task...">
+      <button @click="saveNewTask(newTask)">OK</button>
     </li>
   </ul>
 </div>
@@ -20,18 +28,40 @@
 <script>
 export default {
   data: () => ({
-    tasks: []
+    tasks: [],
+    newTask: {
+      id: null,
+      label: '',
+      completed: false
+    }
   }),
 
   methods: {
     saveTask (task) {
+      task.completed = !task.completed
+
       fetch('/api/', {
         method: 'put',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id: task.id, completed: !task.completed })
+        body: JSON.stringify({ id: task.id, completed: task.completed })
       })
         .then(response => {
           console.log(response)
+        })
+    },
+
+    saveNewTask () {
+      fetch('/api/', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          label: this.newTask.label,
+          completed: this.newTask.completed
+        })
+      })
+        .then(response => {
+          this.tasks.push({ ...this.newTask })
+          this.newTask = Object.assign(this.newTask, { label: '', completed: false })
         })
     }
   },
@@ -58,6 +88,7 @@ export default {
 <style lang="scss">
 .todo-list {
   padding-top: 0;
+  overflow: hidden;
 
   h1 {
     margin: 20px 0 10px;
@@ -66,6 +97,8 @@ export default {
 
   ul {
     list-style-type: none;
+    overflow: auto;
+    max-height: 40vh;
   }
 
   li {
@@ -77,16 +110,11 @@ export default {
     &:hover {background-color: rgba(255, 255, 255, 0.2);}
   }
 
-  input[type="checkbox"] {
-    position: absolute;
-    z-index: -20;
-    visibility: hidden;
-  }
-
   i {
     font-size: 20px;
     padding-top: 3px;
   }
+  .checked i {color: #009688;}
 
   label {
     position: relative;
@@ -104,7 +132,7 @@ export default {
     }
   }
 
-  :checked ~ label {
+  .checked label {
     color: #009688;
 
     &:before {width: 100%;}
@@ -117,19 +145,23 @@ export default {
     border-radius: 99rem;
     background-color: rgba(255, 255, 255, 0.12);
     color: #555;
-    width: 1.8em;
+    width: 1.5rem;
     aspect-ratio: 1;
     display: flex;
     align-items: center;
     justify-content: center;
     margin-left: auto;
     font-size: 0.9rem;
+    font-size: 1rem;
+    transition: 0.3s ease-in-out;
     transition: 0.3s ease-in-out;
 
     &:hover {
       transform: translateX(4px);
       background-color: rgba(255, 255, 255, 0.25);
     }
+
+    &:before {padding-top: 0.2rem;}
   }
 }
 </style>
