fa358ae29beeb688a940fd6c96bf89fa42417b1c
antoniandre <antoniandre.web@gmail.com>
2022-11-18 20:52:59 +0100

    Update the back arrow.

--- frontend/src/components/todo-task.vue
+++ frontend/src/components/todo-task.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="main-content todo-task">
-  <router-link to="/">&lt; Back to list</router-link>
-  <h1>Task #{{ task.id }}</h1>
+  <div class="title-wrap d-flex align-center">
+    <router-link to="/" class="back-arrow i-arrow-left" title="Back to list"></router-link>
+    <h1>Task #{{ task.id }}</h1>
+  </div>
   <p><strong>Label: </strong>{{ task.label }}</p>
   <p><strong>completed: </strong>{{ task.completed }}</p>
 </div>
@@ -36,5 +38,19 @@ export default {
 <style lang="scss">
 .todo-task {
   padding: 40px;
+
+  .title-wrap {margin-bottom: 1rem;}
+
+  .back-arrow {
+    text-decoration: none;
+    color: #000;
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 99em;
+    width: 1.8rem;
+    aspect-ratio: 1;
+    margin-right: 1.5rem;
+
+    &:before {padding-top: 0.2rem;}
+  }
 }
 </style>
