3bc8c7f6b015197c557cebc02ff41ade1900f7be
antoniandre <antoniandre.web@gmail.com>
2024-08-02 13:06:54 +0200

    Upgrade eslint to the latest. :party:

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import standard from "eslint-config-standard";
+import importPlugin from "eslint-plugin-import";
+import nPlugin from "eslint-plugin-n";
+import promisePlugin from "eslint-plugin-promise";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,vue}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  {
+    name: 'standard',
+    rules: standard.rules,
+    plugins: {
+      import: importPlugin,
+      n: nPlugin,
+      promise: promisePlugin
+    }
+  },
+  ...pluginVue.configs["flat/essential"]
+];
