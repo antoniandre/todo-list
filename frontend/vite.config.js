2170d2e4aa33826599181482541405896f234be7
antoniandre <antoniandre.web@gmail.com>
2025-02-07 10:59:18 +0100

    Fix deprecation warning.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        api: 'modern-compiler',
         additionalData: '@import "@/scss/_variables.scss";'
       }
     }
